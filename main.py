9cbd47e015de40012b5fd6b2a6d22b424240337f
pf4-DEV <74263128+elydre@users.noreply.github.com>
2022-05-01 11:36:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+'''    _             _
+  ___ | | _   _   __| | _ __   ___
+ / _ \| || | | | / _` || '__| / _ |
+|  __/| || |_| || (_| || |   |  __/
+ \___||_| \__, | \__,_||_|    \___|
+          |___/
+___________________________________
+
+ - codé en : UTF-8
+ - langage : python3
+ - GitHub  : github.com/elydre
+ - Licence : GNU GPL v3
+'''
+
 import config
 import tkinter as tk
 from tkinter.filedialog import asksaveasfile
